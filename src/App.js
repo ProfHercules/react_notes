ad47ef1603a54f38b311f2e19a6c00c544b558e5
Hercules <hercosmith03@gmail.com>
2020-06-29 19:11:21 +0200

    react hooks introduced

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("Effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("Promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
